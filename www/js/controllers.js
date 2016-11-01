2a2e5637f6b2488b61ef77c97db06d1854de7406
Fili <dany@gmail.com>
2016-10-31 19:00:42 -0500

    un buen me gusta

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,15 +4,16 @@ angular.module('app.controllers', [])
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-
-
+s
 }])
    
 .controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams) 
 
 
+
+a
 }])
  
